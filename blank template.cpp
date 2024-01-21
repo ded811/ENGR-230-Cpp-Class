1d84897ac8ba63682df91c873a193a9575d3fb6c
ded811 <ajzimmermannapple@gmail.com>
2024-01-20 19:10:40 -0600

    yes

---
+++ blank template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "HW1 Course Info Program" << endl <<endl
+        << "Course: ENGR230" <<endl
+        << "Description: Programming Engineering Applications in C++" <<endl
+        << "Instructor: Steven Portscheller" <<endl
+        << "Office: 318D" <<endl
+        << "Phone: (309) 694-8449" <<endl
+        << "Email: steven.portscheller@icc.edu" <<endl;
+    return 0;
+}
